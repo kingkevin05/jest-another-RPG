e17dbedc9894b299c834d1c6b0a41d6b41119a6a
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-04-03 10:42:26 -0700

    added questions

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,7 @@
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
+
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
