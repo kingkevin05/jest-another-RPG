d79e501ca05f73c241d253d90f1188f3b83fbc3f
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-04-08 09:59:23 -0700

    added prototypes

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -15,3 +15,22 @@ test('create a player object', () => {
     expect.arrayContaining([expect.any(Object)])
   );
 });
+
+test("gets player's stats as an object", () => {
+  const player = new Player('Kevin');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
